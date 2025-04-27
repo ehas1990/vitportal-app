dd3f544b1c934b4fa82ef3ff308acd08a6b9377b
ehas1990 <ehasahamed@gmail.com>
2025-04-27 13:23:00 +0530

    current_learning_update

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,17 @@
-import {forwardRef, React }from 'react'
+import {forwardRef, useImperativeHandle,useRef}from 'react'
 
 const ResultModal = forwardRef(function ResultModal({result, targetTime},ref) {
+ const dialog=useRef();
+  useImperativeHandle(ref,()=>{
+    return{
+      open(){
+        dialog.current.showModal();
+      }
+    }
+  });
   return (
     
-    <dialog ref={ref} className='result-modal'>
+    <dialog ref={dialog} className='result-modal'>
         <form method='dialog'>
             <button>X</button>
         </form>
